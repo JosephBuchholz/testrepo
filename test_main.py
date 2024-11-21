d1cca264c7356677a7bc5523e6c2d3689de3fca7
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2024-11-21 10:31:31 -0800

    Added subtraction function

--- test_main.py
+++ test_main.py
@@ -1,6 +1,12 @@
-from main import add
+from main import add, sub
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+def test_sub():
+    assert sub(2, 3) == 5
+    assert sub(-1, 1) == 0
+    assert sub(0, 0) == 0
+
